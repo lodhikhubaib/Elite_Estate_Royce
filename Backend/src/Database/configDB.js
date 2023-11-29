8e034cdef0eb26fd93056d481c941903a10f7ee9
Khubaib Lodhi <lodhikhubaib12@gmail.com>
2023-11-29 22:58:45 +0500

    DB connection

---
+++ Backend/src/Database/configDB.js
@@ -0,0 +1,20 @@
+var mysql = require('mysql');
+
+var conn = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"root"
+});
+
+
+/*var conn = mysql.createConnection({
+    host:"localhost",
+    user:"root",
+    password:"Lodhi123"
+});*/
+
+conn.connect(function(err){
+    if(err)
+        throw err;
+    console.log("Connection Successful...");
+});
